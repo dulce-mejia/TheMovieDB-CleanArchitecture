cb54cb37f88db021acff1c361f9f9a388c2742c9
Dulce Gardenia Mejia Aguayo <dulce.mejia@wizeline.com>
2022-06-21 15:23:01 -0500

    Initial Commit

---
+++ TheMovieDB-CleanArchitecture/Core/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TheMovieDB-CleanArchitecture
+//
+//  Created by Dulce Mejia Aguayo on 21/06/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    let appName = "TheMovieDB"
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+    
+    private func setupRootViewController(_ rootViewController: UIViewController) {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = rootViewController
+        self.window?.makeKeyAndVisible()
+    }
+}
+
