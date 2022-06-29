9d6b49512cd3150f7c0dcc263bc3915dd8995255
Dulce Gardenia Mejia Aguayo <dulce.mejia@wizeline.com>
2022-06-29 12:11:02 -0500

    domain layer for Reviews Module added

---
+++ TheMovieDB-CleanArchitecture/Domain/Reviews/UseCases/ReviewsLoader.swift
@@ -0,0 +1,14 @@
+//
+//  ReviewsLoader.swift
+//  TheMovieDB-CleanArchitecture
+//
+//  Created by Dulce Mejia Aguayo on 29/06/22.
+//
+
+import Foundation
+
+public protocol ReviewsLoader {
+    typealias Result = Swift.Result<GenericResult<Review>?, Error>
+    
+    func load(movieId: Int, completion: @escaping (Result) -> Void)
+}
